7ca867136c372b327f1e309145a4060bee637003
Huỳnh Hữu Dũng <huudung0607@gmail.com>
2025-01-12 19:14:50 +0700

    Create Chèn.cpp

---
+++ Chèn.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <cmath>
+#include <numeric>
+#include <climits>
+#include <iomanip>
+#include <string>
+using namespace std;
+const int mod = 1e9 + 7;
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	string s;
+	cin >> s;
+	string chen = "28tech";
+	int k;
+	cin >> k;
+	s.insert(k, chen);
+	cout << s;
+}
