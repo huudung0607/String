7c0727c2de0d93655199f4a39242b9389ac9b033
Huỳnh Hữu Dũng <huudung0607@gmail.com>
2025-01-13 18:33:46 +0700

    Create to_string.cpp

---
+++ to_string.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <cmath>
+#include <numeric>
+#include <climits>
+#include <iomanip>
+#include <string>
+#include <sstream>
+using namespace std;
+const int mod = 1e9 + 7;
+string my_to_string(long long n) {
+	return to_string(n);
+}
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	long long n; cin >> n;
+	cout << my_to_string(n) << endl;
+}
