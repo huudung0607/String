0c0e17a7e1aae99d75fb638e11de540f6903b3aa
Huỳnh Hữu Dũng <huudung0607@gmail.com>
2025-01-15 17:53:45 +0700

    Create Xâu đối xứng.cpp

---
+++ Xâu đối xứng.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <cmath>
+#include <numeric>
+#include <climits>
+#include <iomanip>
+#include <string>
+#include <sstream>
+using namespace std;
+const int mod = 1e9 + 7;
+bool check(string s)
+{
+	int l = 0, r = s.size() - 1;
+	while (l <= r)
+	{
+		if (s[l] != s[r])
+		{
+			return false;
+		}
+		l++;
+		r--;
+	}
+	return true;
+}
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	string s;
+	cin >> s;
+	if (check(s))
+	{
+		cout << "YES";
+	}
+	else
+	{
+		cout << "NO";
+	}
+}
