bc889421d5b09a9318ed4b7de43d12c574117344
Huỳnh Hữu Dũng <huudung0607@gmail.com>
2025-01-16 17:23:12 +0700

    Create Tập từ riêng của 2 xâu.cpp

---
+++ Tập từ riêng của 2 xâu.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <cmath>
+#include <numeric>
+#include <climits>
+#include <iomanip>
+#include <string>
+#include <sstream>
+using namespace std;
+const int mod = 1e9 + 7;
+string mail = "@xyz.edu.vn";
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	string t;
+	getline(cin, t);
+	string s;
+	getline(cin, s);
+	for (char& x : t)
+	{
+		x = tolower(x);
+	}
+	for (char& x : s)
+	{
+		x = tolower(x);
+	}
+	stringstream ss1(t), ss2(s);
+	string temp1, temp2;
+	set<string> s1, s2;
+	while (ss1 >> temp1)
+	{
+		s1.insert(temp1);
+	}
+	while (ss2>>temp2)
+	{
+		s2.insert(temp2);
+	}
+	map<string, int> mp;
+	for (auto x : s1)
+	{
+		mp[x] = 1;
+	}
+	for (auto x : s2)
+	{
+		if (mp[x] == 1)
+		{
+			mp[x] = 2;
+		}
+	}
+	for (auto x : s1)
+	{
+		if (mp[x] != 2)
+		{
+			cout << x << " ";
+		}
+	}
+}
