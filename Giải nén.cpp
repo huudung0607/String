e735127ec80b93dffc187fd725e4b0dcfcd832d3
Huỳnh Hữu Dũng <huudung0607@gmail.com>
2025-01-24 11:04:32 +0700

    Create  Giải nén.cpp

---
+++ Giải nén.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <cmath>
+#include <numeric>
+#include <climits>
+#include <iomanip>
+#include <string>
+#include <sstream>
+#include <queue>
+#include <stack>
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	int n;
+	char c;
+	while (cin >> c >> n)
+	{
+		for (int i = 0; i < n; i++)
+		{
+			cout << c;
+		}
+	}
+}
